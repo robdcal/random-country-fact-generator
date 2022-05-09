1b5e1befb25ab08ad27bae94f6ca0d66f2fc10bc
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-09 22:57:43 +0100

    setting up initial API call and rendering of capital city fact

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FactCard from "./components/FactCard";
 
 const App = () => {
+  const [countryData, setCountryData] = useState([]);
   const [factData, setFactData] = useState({
     factText: null,
   });
 
   const handleClick = () => {
-    console.log("handleClick");
-    console.log(factData);
+    // console.log("handleClick");
     setFactData(getFact());
+    // console.log(factData);
   };
 
   const getFact = () => {
-    console.log("getFact");
-    console.log(factData);
-    return { factText: "this is a new fact" };
+    // console.log("getFact");
+    const countryNum = Math.floor(Math.random() * countryData.length) + 1;
+    // console.log(factData);
+    return {
+      factText: `The capital of ${countryData[countryNum].name.common} is ${countryData[countryNum].capital[0]}.`,
+    };
   };
 
-  // setup template / input data (country codes/names, returned text strings, etc)
-  // request data from API using random selection of COUNTRY + DATAPOINT
-  // Display country flag and data from API using template text
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryData(data);
+        // setFactData(getFact());
+        // console.log(factData);
+      });
+  }, []);
 
   return (
     <div className="App">
