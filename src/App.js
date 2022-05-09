1a7b8bbc6fa0b17c8db7a2382c5491cfbf652abc
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-09 09:41:18 +0100

    setting up basic logic for displaying string

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import FactCard from "./components/FactCard";
 
-function App() {
+const App = () => {
+  const [factData, setFactData] = useState({
+    factText: null,
+  });
+
+  const handleClick = () => {
+    console.log("handleClick");
+    console.log(factData);
+    setFactData(getFact());
+  };
+
+  const getFact = () => {
+    console.log("getFact");
+    console.log(factData);
+    return { factText: "this is a new fact" };
+  };
+
+  // setup template / input data (country codes/names, returned text strings, etc)
+  // request data from API using random selection of COUNTRY + DATAPOINT
+  // Display country flag and data from API using template text
+
   return (
     <div className="App">
       <header className="hero">
         <h1>Random Country Fact Generator</h1>
+        <button onClick={handleClick}>Generate Fact</button>
+        <FactCard factData={factData} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
